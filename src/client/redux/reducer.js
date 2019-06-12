e8ef13f7eceb70739e4cb6e1d05dd1820d858c19
1482223229 <32259811+1482223229@users.noreply.github.com>
2019-06-12 15:58:27 +0800

    Update reducer.js

--- src/client/redux/reducer.js
+++ src/client/redux/reducer.js
@@ -1,6 +1,8 @@
 import { TOLOGIN, logaction } from './action.js';
 import { createStore, ReducersMapObject } from 'redux';
-const todoapp = ((state = {}, action) => {
+let initstate = { // 初始化数据
+}
+const todoapp = ((initstate, action) => {
     switch (action.type) {
         case TOLOGIN :
                  window.localStorage.setItem('lagouwang', JSON.stringify(action.val) );
