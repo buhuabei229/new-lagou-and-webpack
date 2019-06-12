99bce22d1f74146729f8eb6dc3029485c8eae93a
1482223229 <32259811+1482223229@users.noreply.github.com>
2019-06-12 15:51:46 +0800

    Update reducer.js

--- src/client/redux/reducer.js
+++ src/client/redux/reducer.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
 import { TOLOGIN, logaction } from './action.js';
-import { connect, Provider } from 'react-redux';
 import { createStore, ReducersMapObject } from 'redux';
-import Login from '../components/login/login.js';
-import Mock from 'mockjs';
 const todoapp = ((state = {}, action) => {
     switch (action.type) {
         case TOLOGIN :
-                 Mock.mock('/login/local', action.val);
                  window.localStorage.setItem('lagouwang', JSON.stringify(action.val) );
                  console.log(action);
             break;
-    
         default:
             break;
     }
 })
-const store = createStore(todoapp);
-const Todologin = connect()(Login);
-class ReduxLogin extends Component {
-    render(){
-        return (
-            <Provider store={store}>
-               <Todologin />
-            </Provider>
-        )
-    }
-}
 export default ReduxLogin;
