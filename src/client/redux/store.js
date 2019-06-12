8cbf31f30eb97e06089a8d0e5a9250ecf873d9b4
1482223229 <32259811+1482223229@users.noreply.github.com>
2019-06-12 15:49:51 +0800

    Update store.js

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,11 +1,5 @@
+import todoapp from './reduser.js'
+import { Provider } from 'react-redux';
+import { createStore, ReducersMapObject } from 'redux';
 const store = createStore(todoapp);
-const Todologin = connect()(Login);
-class ReduxLogin extends Component {
-    render(){
-        return (
-            <Provider store={store}>
-               <Todologin />
-            </Provider>
-        )
-    }
-}
+export default store
