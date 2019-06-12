6f3f07cb1da7fdc8b74b23ddaf4ae1f08b4697f8
1482223229 <32259811+1482223229@users.noreply.github.com>
2019-06-12 15:53:30 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './client/components/App/App.js';
+
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
+            <Provider store={store}>
+              <App/>
+            </Provider>,
+            document.getElementById('root')
 )
